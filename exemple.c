4c6183cb2e27752b49b592a4950670d5b0cf3232
emie <lalala.emiliat@gmail.com>
2021-02-21 11:59:56 +0100

    Boucle do while avec pointeur chaine de caractère

--- exemple.c
+++ exemple.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
-{
+{	
+	char  * chaine;
+	int un;
+	chaine = malloc(sizeof(char *) * 50);
 	int nombre = 0;
 	for ( int i = 0;i<4;i++)
 	{
 		printf("nombre :%d\n",nombre);
 		nombre ++;
 	}
-	int jeu = 10 ;
-	int plus;
-	printf("10 + ");
-	scanf("%d",&plus);
-	jeu = 10 + plus;
-	printf("= %d\n",jeu);
+	
+	do
+	{
+		
+		printf("Tapez '1' pour sortir\n");
+		fgets(chaine,50,stdin);
+		chaine[strlen(chaine)-1] = '\0';
+		un = atoi(chaine);
+
+	}while(un != 1);
+
+	return 0;
+ 
+	
 }
